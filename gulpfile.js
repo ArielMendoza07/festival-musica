55c358ffd625ccf3f26663f243bd02b0339abb8a
ArielMendoza07 <angeloshady07@gmail.com>
2022-08-16 01:50:59 -0500

    Three bastard commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+//IMG
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   src("src/scss/**/*.scss")
@@ -11,11 +19,49 @@ function css(done) {
   done();
 }
 
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
+  src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("build/img"));
+
+  done();
+}
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+  done();
+}
+
+function versionAvif(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(avif(opciones)).pipe(dest("build/img"));
+  done();
+}
+
+function javascript(done){
+  src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+  done()
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", javascript);
 
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionAvif = versionAvif;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
